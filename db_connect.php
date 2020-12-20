b364cd88e795a019fe67eda0c7969e60b073db67
sthai3179 <76411388+sthai3179@users.noreply.github.com>
2020-12-20 16:42:14 -0700

    Update to Heroku JawsDB

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,10 @@
 <?php
 
 //4 variables to connect to database
-$host = "localhost";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "jokes";
+$host = "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "qf0e4ielcdj4zufh";
+$user_pass = "dunwz8jskpe2tzy1";
+$database_in_use = "qsfxvkbv5owllp4b";
 
 //create database connection instance
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
